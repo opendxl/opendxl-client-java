e80e7dc5ba92066a88f90a218b10f4fdffa575f9
Kris Leonard <kristopher_leonard@mcafee.com>
2019-07-03 11:25:44 -0700

    Added proxy usage verification test and fixed issues in the travis ci script.

---
+++ src/test/java/com/opendxl/client/ProxyUsageVerificationTest.java
@@ -0,0 +1,36 @@
+package com.opendxl.client;
+
+import com.opendxl.client.exception.DxlException;
+import com.opendxl.client.testutil.impl.DxlClientImplFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ProxyUsageVerificationTest {
+
+    @Test
+    public void verifyProxyUsage() throws DxlException {
+
+        DxlClientImplFactory dxlClientImplFactory = DxlClientImplFactory.getDefaultInstance();
+        DxlClientConfig config = dxlClientImplFactory.getConfig();
+
+        int originalProxyPort = config.getProxyPort();
+
+        config.setProxyPort(originalProxyPort - 10);
+        try (DxlClient client = new DxlClient(config)) {
+            client.getConfig().setConnectRetries(1);
+            client.connect();
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Unable to connect to server: Connection refused: connect"));
+        }
+
+        config.setProxyPort(originalProxyPort);
+        try (DxlClient client = new DxlClient(config)) {
+            client.getConfig().setConnectRetries(1);
+            client.connect();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
